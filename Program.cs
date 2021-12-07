936954d2153cb48e15239bd04af96298379fa801
Your Name <you@example.com>
2021-12-08 02:45:17 +0500

    first

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Inetlab.SMPP;
+using Inetlab.SMPP.Common;
+using Inetlab.SMPP.Logging;
+using Inetlab.SMPP.PDU;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogManager.SetLoggerFactory(new ConsoleLogFactory(LogLevel.Verbose));
+
+
+            SendHelloWorld().Wait();
+        }
+
+        // <Послать HelloWorld>
+        public static async Task SendHelloWorld()
+        {
+            using (SmppClient client = new SmppClient())
+            {
+
+                if (await client.ConnectAsync("127.0.0.1", 7777))
+                {
+                    BindResp bindResp = await client.BindAsync("username", "password");
+                    
+                    for (int i = 0; i<500; i++){
+                        if (bindResp.Header.Status == CommandStatus.ESME_ROK)
+                        {
+                            var submitResp = await client.SubmitAsync(
+                                SMS.ForSubmit()
+                                    .From("111")
+                                    .To("222")
+                                    .Coding(DataCodings.UCS2)
+                                    .Text($"Hello World! {i}"));
+
+                            if (submitResp.All(x => x.Header.Status == CommandStatus.ESME_ROK))
+                            {
+                                client.Logger.Info("Сообщение было отправлено.");
+                            }
+                        }
+                    }
+
+                    await client.DisconnectAsync();
+                }
+            }
+        }
+        //</Послать HelloWorld>
+    }
+}
