cd54a69a2baf45f75fae055e0293e21583758eb8
server <aoaoa@aoaoa.aoao>
2021-12-08 03:39:53 -0500

    smpp

--- Program.cs
+++ Program.cs
@@ -26,18 +26,19 @@ namespace HelloWorld
         {
             using (SmppClient client = new SmppClient())
             {
+		    client.ConnectionRecovery = true;
 
-                if (await client.ConnectAsync("127.0.0.1", 7777))
+                if (await client.ConnectAsync("10.84.33.102", 900))
                 {
-                    BindResp bindResp = await client.BindAsync("username", "password");
+                    BindResp bindResp = await client.BindAsync("PaymonaS", "1xVpS3n5");
                     
                     for (int i = 0; i<500; i++){
                         if (bindResp.Header.Status == CommandStatus.ESME_ROK)
                         {
                             var submitResp = await client.SubmitAsync(
                                 SMS.ForSubmit()
-                                    .From("111")
-                                    .To("222")
+                                    .From("00009011", (AddressTON)0, (AddressNPI)1)
+                                    .To("992928560139", (AddressTON)1, (AddressNPI)1)
                                     .Coding(DataCodings.UCS2)
                                     .Text($"Hello World! {i}"));
 
@@ -48,8 +49,7 @@ namespace HelloWorld
                         }
                     }
 
-                    await client.DisconnectAsync();
-                }
+                         }
             }
         }
         //</Послать HelloWorld>
