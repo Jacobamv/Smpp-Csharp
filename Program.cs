17e4c66b7eff2d6ca2904eaffe695388d1400801
Jacob Akhmedov <56736396+Jacobamv@users.noreply.github.com>
2022-03-27 17:24:59 +0500

    bug

--- Program.cs
+++ Program.cs
@@ -28,17 +28,17 @@ namespace HelloWorld
             {
 		    client.ConnectionRecovery = true;
 
-                if (await client.ConnectAsync("10.84.33.102", 900))
+                if (await client.ConnectAsync("host", "port"))
                 {
-                    BindResp bindResp = await client.BindAsync("PaymonaS", "1xVpS3n5");
+                    BindResp bindResp = await client.BindAsync("Login", "Password");
                     
                     for (int i = 0; i<500; i++){
                         if (bindResp.Header.Status == CommandStatus.ESME_ROK)
                         {
                             var submitResp = await client.SubmitAsync(
                                 SMS.ForSubmit()
-                                    .From("00009011", (AddressTON)0, (AddressNPI)1)
-                                    .To("992928560139", (AddressTON)1, (AddressNPI)1)
+                                    .From("number", (AddressTON)0, (AddressNPI)1)
+                                    .To("number", (AddressTON)1, (AddressNPI)1)
                                     .Coding(DataCodings.UCS2)
                                     .Text($"Hello World! {i}"));
 
